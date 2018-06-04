47efb1bb34003fba0d1ab1d25eef197ab465d51b
mercykwazi <mercymncube3@gmail.com@gmail.com>
2018-06-04 14:05:44 +0200

    added a third test that test whether the calculator will return zero when it passed a string

--- 04-06-2018-calculator/string-calculator.js
+++ 04-06-2018-calculator/string-calculator.js
@@ -1,20 +1,33 @@
+
+
 function addString(string) {
-    var spited = string.split(',');
-    var arr = [];
-
-    for (var i = 0; i < spited.length; i++) {
-        arr.push(spited[i])
-        if (arr[i] == "") {
-            return 0;
-        };
+    if (string == "") {
+        return 0;
+    } else {
+        if (/(\d).(\d)/.exec(string) || /(-\d)/.exec(string)) {
+            var numbers = string.match(/(-\d)|(\d)[.](\d)|(\d)(\d)|([0-9])/g);
+        }
+        for (var i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                var message = `Negative numbers not allowed: ${numbers}`;
+                throw Error(message);
+            }
+        }
+        var sum = numbers.reduce(function (a, b) {
+            return Number(a) + Number(b);
+        });
+        return sum;
     }
-    var sum = arr.reduce(function (a, b) {
-        return parseInt(a) + parseInt(b);
-    });
-
-
-    return sum;
 }
 
-console.log(addString("1,2"))
+// console.log(addString("1,2"))
+
+
+// console.log(addInt("1,2"))
+// console.log(addInt("10,17"))
+// console.log(addInt(""))
+// console.log(addInt("1\n2,3"));
+// console.log(addInt("//;\n2;6;9"))
+// console.log(addInt("//,\n2.5,2.2"))
+// console.log(addInt("-1,-2"))
 module.exports={addString}
--- 04-06-2018-calculator/test/test.js
+++ 04-06-2018-calculator/test/test.js
@@ -11,5 +11,8 @@ describe("String Calculator", function () {
     it("should return the sum of to numbers passed", function () {
         expect(calculator.addString("1,2")).to.be.eql(3);
     });
+    it("should return zero when '' is passed", function(){
+        expect(calculator.addString('')).to.be.eql(0);
+      });
 });
 // 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
