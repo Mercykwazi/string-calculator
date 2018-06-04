1af91c02048415773738fb7f060b43aac8b60855
mercykwazi <mercymncube3@gmail.com@gmail.com>
2018-06-04 14:55:47 +0200

    added a test that checks wheather a n is escaped

--- 04-06-2018-calculator/test/test.js
+++ 04-06-2018-calculator/test/test.js
@@ -11,8 +11,13 @@ describe("String Calculator", function () {
     it("should return the sum of to numbers passed", function () {
         expect(calculator.addString("1,2")).to.be.eql(3);
     });
-    it("should return zero when '' is passed", function(){
+    it("should return zero when a string  is passed", function(){
         expect(calculator.addString('')).to.be.eql(0);
       });
+
+    it("should allow \\n  in the string culculator", function(){
+       expect (calculator.addString("1\n2,3")).to.be.eql(6);
+    });
+
 });
 // 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
